49b29ce8e28e7c0f3ab7033a95682c0c40478909
Erik Oliveira Leão <102800566+erik-oliveira-leao@users.noreply.github.com>
2022-09-20 19:56:03 -0300

    Add files via upload

---
+++ atividade.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void)
+{
+  int i,igual=0;
+  
+  char texto[6] = "texto"; //altere esta palavra por outra do mesmo tamanho
+  char texto2 [6] = "preco"; //altere esta palavra por outra do mesmo tamanho
+  
+  printf("Valor da primeira variavel = %s\n", texto);
+  printf("\nValor da segunda variavel = %s\n", texto2);
+  
+ if (texto[0]==texto2[0]){
+ 	igual++;
+ }else{
+ }
+ if (texto[0]==texto2[1]){
+ 	igual++;
+ }else{
+ }
+ if (texto[0]==texto2[2]){
+ 	igual++;
+ }else{
+ }
+ if (texto[0]==texto2[3]){
+ 	igual++;
+ }else{
+ }
+ if (texto[0]==texto2[4]){
+ 	igual++;
+ }else{
+ }
+ if (texto[1]==texto2[0]){
+ 	igual++;
+ }else{
+ }
+ if (texto[1]==texto2[1]){
+ 	igual++;
+ }else{
+ }
+ if (texto[1]==texto2[2]){
+ 	igual++;
+ }else{
+ }
+ if (texto[1]==texto2[3]){
+ 	igual++;
+ }else{
+ }
+ if (texto[1]==texto2[4]){
+ 	igual++;
+ }else{
+ }
+ if (texto[2]==texto2[0]){
+ 	igual++;
+ }else{
+ }
+ if (texto[2]==texto2[1]){
+ 	igual++;
+ }else{
+ }
+ if (texto[2]==texto2[2]){
+ 	igual++;
+ }else{
+ }
+ if (texto[2]==texto2[3]){
+ 	igual++;
+ }else{
+ }
+ if (texto[2]==texto2[4]){
+ 	igual++;
+ }else{
+ }
+ if (texto[3]==texto2[0]){
+ 	igual++;
+ }else{
+ }
+ if (texto[3]==texto2[1]){
+ 	igual++;
+ }else{
+ }
+ if (texto[3]==texto2[2]){
+ 	igual++;
+ }else{
+ }
+ if (texto[3]==texto2[3]){
+ 	igual++;
+ }else{
+ }
+ if (texto[3]==texto2[4]){
+ 	igual++;
+ }else{
+ }
+ if (texto[4]==texto2[0]){
+ 	igual++;
+ }else{
+ }
+ if (texto[4]==texto2[1]){
+ 	igual++;
+ }else{
+ }
+ if (texto[4]==texto2[2]){
+ 	igual++;
+ }else{
+ }
+ if (texto[4]==texto2[3]){
+ 	igual++;
+ }else{
+ }
+ if (texto[4]==texto2[4]){
+ 	igual++;
+ }else{
+ }
+ 
+ if (igual>0){
+ 	printf("\nAs palavras contem %d letras iguais",igual);
+ }else{
+ 	printf("\nAs palavras contem %d letras iguais",igual);
+ }
+        
+        
+         printf("\n=================================================\n") ;
+         
+         
+        
+  getch();
+  return 0;
+}
